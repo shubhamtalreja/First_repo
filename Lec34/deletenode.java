134caeb7be18f09407a3d4c4b91d48bdf408eb8f
shubhamtalreja <shubhamtalreja2@gmail.com>
2022-03-14 18:58:08 +0530

    Linked List

---
+++ Lec34/deletenode.java
@@ -0,0 +1,19 @@
+package Lec34;
+
+	public class deletenode {
+		public class ListNode {
+			      int val;
+			      ListNode next;
+			      ListNode(int x) { val = x; }
+			 
+			
+			class Solution {
+			    public void deleteNode(ListNode node) {
+			        node.val = node.next.val;
+			        node.next = node.next.next;
+			        
+			    }
+			}
+		}
+	}
+
